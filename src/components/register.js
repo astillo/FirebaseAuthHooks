c135b871ac5562dd57b9d2134b152bd100e608a4
astillo <clomes.alex@gmail.com>
2019-11-13 16:22:45 -0600

    need to handle submit on reg

---
+++ src/components/register.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { Form, Icon, Input, Button, Card } from "antd";
+import { Container } from "../styles/styled";
+
+const Register = props => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+  };
+  return (
+    <Container>
+      <Card
+        title="Firebase Hooks Tutorial Register"
+        style={{ width: 300, textAlign: "center" }}
+      >
+        <Form onSubmit={handleSubmit}>
+          <Input
+            prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
+            placeholder="Email"
+            style={{ marginBottom: "10px" }}
+            onChange={e => {
+              setEmail(e.target.value);
+            }}
+          />
+          <Input
+            prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />}
+            type="password"
+            placeholder="Password"
+            style={{ marginBottom: "10px" }}
+            onChange={e => {
+              setPassword(e.target.value);
+            }}
+          />
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+            style={{ width: "100%", marginBottom: "10px" }}
+          >
+            Register
+          </Button>
+          Or
+          <Button
+            type="primary"
+            onClick={() => {
+              props.history.push("/login");
+            }}
+            style={{ width: "100%", marginTop: "10px" }}
+          >
+            Login
+          </Button>
+        </Form>
+      </Card>
+    </Container>
+  );
+};
+
+export default Register;
